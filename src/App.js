0dafac7e75a898f6b4c41b4382413ba446a54fdc
Raymond Yeh <ray@geek.sg>
2021-07-20 17:15:51 +0800

    feat: updated colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+// import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+// import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+// import { ReactComponent as IconGithub } from './assets/icons/github.svg';
 import "./App.css";
 
 class App extends React.Component {
@@ -10,10 +10,10 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">BLUEJAY</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
             <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
@@ -21,7 +21,7 @@ class App extends React.Component {
             </a>
             <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
-            </a>
+            </a> */}
           </div>
         </div>
         <div className="content">
@@ -29,12 +29,11 @@ class App extends React.Component {
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:sherry@bluejay.finance">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
